a0c062d5d306432bfad3c110b1ac934effc8c9dc
rosch-arthur <rosch.arthur@gmail.com>
2024-11-26 09:28:43 -0300

    Fix error in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,57 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
-import express, { Application, Request, Response } from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import express, { Application } from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from './resolvers/UserResolver';
 import { PostResolver } from './resolvers/PostResolver';
 import { CommentResolver } from './resolvers/CommentResolver';
 import { authChecker } from './utils/authChecker';
-import { Context } from './types/Context';
 import { verifyToken } from './utils/auth';
-import cors from 'cors'; 
-import './types/type-graphql';
+import cors from 'cors';
 
 dotenv.config();
 
 async function bootstrap() {
   const app: Application = express();
 
-  // Configuração do CORS
-  const corsOpts = {
-    origin: '*',  // Permite todas as origens, pode ser alterado para um domínio específico
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Cabeçalhos permitidos
-    credentials: true,  // Permite enviar cookies e credenciais, se necessário
-  };
+  
+  app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
 
-  // Aplica o CORS ao Express
-  app.use(cors(corsOpts));
-
+  
   const schema = await buildSchema({
     resolvers: [UserResolver, PostResolver, CommentResolver],
     authChecker,
   });
 
-  const server = new ApolloServer({
-    schema,
-    context: ({ req }): Context => {
-      const token = req.headers.authorization?.split(' ')[1];
-      const user = token ? verifyToken(token) : null;
-      return { user };
-    },
-    introspection: true,  // Permite introspecção em ambientes de desenvolvimento
-  });
+  
+  const server = new ApolloServer({ schema });
 
   await server.start();
-  
-  // Aplica o middleware do Apollo Server ao Express
-  server.applyMiddleware({ app });
 
+  
+  app.use(
+    '/graphql',
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const token = req.headers.authorization?.split(' ')[1];
+        const user = token ? verifyToken(token) : null;
+        return { user };
+      },
+    }),
+  );
+
+  
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
   });
 }
 
